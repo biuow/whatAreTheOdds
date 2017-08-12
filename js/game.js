e63225e31d10b353ff2b560d991d09d7af225bb6
Sofia <sofibareta@gmail.com>
2017-08-12 18:20:24 -0300

    teste

--- js/game.js
+++ js/game.js
@@ -1,73 +1,73 @@
-var jogaCartas = function(){
+var jogaCartas = function() {
 
-	//Define as cartas
-	var card1 = Math.trunc(Math.random()*13);
-	var card2 = Math.trunc(Math.random()*13);
+    //Define as cartas
+    var card1 = Math.trunc(Math.random() * 13);
+    var card2 = Math.trunc(Math.random() * 13);
 
-	//Define as imagens
+    //Define as imagens
 
-	if(card1 >= 10)
-		card1 = 10;
-	if(card2 >= 10)
-		card2 = 10;
+    if (card1 >= 10)
+        card1 = 10;
+    if (card2 >= 10)
+        card2 = 10;
 
-	while(card1+card2+10 == 21){
-		card1 = Math.random()*13;
-		card2 = Math.random()*13;
+    while (card1 + card2 + 10 == 21) {
+        card1 = Math.random() * 13;
+        card2 = Math.random() * 13;
 
-		//Define as imagens
+        //Define as imagens
 
-		if(card1 >= 10)
-			card1 = 10;
-		if(card2 >= 10)
-			card2 = 10;
-	}
+        if (card1 >= 10)
+            card1 = 10;
+        if (card2 >= 10)
+            card2 = 10;
+    }
 
-	//Calcula a chance
-	var falta = 21 - (card1+card2);
-	var faltaBar = 0;
+    //Calcula a chance
+    var falta = 21 - (card1 + card2);
+    var faltaBar = 0;
 
-	if(falta > card1)
-		faltaBar++;
-	if(falta > card2)
-		faltaBar++;
+    if (falta > card1)
+        faltaBar++;
+    if (falta > card2)
+        faltaBar++;
 
-	var chance = (falta*4) - faltaBar;
+    var chance = (falta * 4) - faltaBar;
 
-	chance = chance/52*100;
+    chance = chance / 52 * 100;
 
-	if(chance>100)
-		chance = 100;
+    if (chance > 100)
+        chance = 100;
 
-	document.getElementById("idText").text = Math.trunc(chance) + "%";
+    document.getElementById("idText").text = Math.trunc(chance) + "%";
 }
 
-var jogaDados1 = function(){
+var jogaDados1 = function() {
 
-	//Define as cartas
-	var dice1 = Math.trunc(Math.random()*6);
+    //Define as cartas
+    var dice1 = Math.trunc(Math.random() * 6);
 
-	//Define as imagens
+    //Define as imagens
 
-	chance = 1/6;
+    chance = 1 / 6;
 
-	document.getElementById("idText").text = Math.trunc(chance) + "%";
+    document.getElementById("idText").text = Math.trunc(chance) + "%";
 }
 
-var jogaDados2 = function(){
+var jogaDados2 = function() {
 
-	//Define as cartas
-	var dice1 = Math.trunc(Math.random()*6);
+    //Define as cartas
+    var dice1 = Math.trunc(Math.random() * 6);
 
-	//Define as imagens
+    //Define as imagens
 
-	//Calcula a chance
-	var falta = 7 - (dice1);
+    //Calcula a chance
+    var falta = 7 - (dice1);
 
-	var chance = falta/6*100;
+    var chance = falta / 6 * 100;
 
-	if(chance>100)
-		chance = 100;
+    if (chance > 100)
+        chance = 100;
 
-	document.getElementById("idText").text = Math.trunc(chance) + "%";
+    document.getElementById("idText").text = Math.trunc(chance) + "%";
 }
